07c556486b994ee24734304fb032ecf28c9151db
Rood <48691039+Babaloopey@users.noreply.github.com>
2023-03-29 20:33:22 +0200

    feat: New Button to download all plans

--- gui.py
+++ gui.py
@@ -8,22 +8,26 @@ import team_games
 
 root = project_variables.root
 
+club = ""
+teams = []
 
 # calculates how big the gui has to be
 def lines_in_gui(teams):
     button_height = 50
-    print(math.ceil(len(teams) / 3))
     return math.ceil(len(teams) / 3) * button_height
 
 
 def create_gui():
+    global club
+    global teams
+
     for widget in root.winfo_children():
         widget.destroy()
 
     teams = project_variables.update_teams()
-    club_name = teams.iloc[0]
+    club = teams.iloc[0]
 
-    canvas = tk.Canvas(root, height=(70 + lines_in_gui(teams)), width=650)
+    canvas = tk.Canvas(root, height=(100 + lines_in_gui(teams)), width=650)
     canvas.pack()
 
     title_frame = tk.Frame(root)
@@ -41,7 +45,7 @@ def create_gui():
     column3 = tk.Frame(root)
     column3.place(relwidth=0.25, relheight=0.75, relx=0.7, y=70)
 
-    greeting = tk.Label(title_frame, text=club_name[0], font=("Calibri", 25))
+    greeting = tk.Label(title_frame, text=club[0], font=("Calibri", 25))
     greeting.pack()
 
     btn_configuration = tk.Button(config_frame, text="Konfiguration", fg="black", bg="#eddcd9",
@@ -54,18 +58,21 @@ def create_gui():
             tk.Button(column1, text=team[1].description, padx=10, pady=10, fg="white",
                       bg=project_variables.button_colour,
                       width=50,
-                      command=lambda c=team: call_distributor(c, club_name)).pack()
+                      command=lambda reference=team: call_distributor(reference)).pack()
         elif team[0] < (len(teams) / 3 * 2):
             tk.Button(column2, text=team[1].description, padx=10, pady=10, fg="white",
                       bg=project_variables.button_colour,
                       width=50,
-                      command=lambda c=team: call_distributor(c, club_name)).pack()
+                      command=lambda reference=team: call_distributor(reference)).pack()
         elif team[0] < (len(teams) / 3 * 3):
             tk.Button(column3, text=team[1].description, padx=10, pady=10, fg="white",
                       bg=project_variables.button_colour,
                       width=50,
-                      command=lambda c=team: call_distributor(c, club_name)).pack()
-
+                      command=lambda reference=team: call_distributor(reference)).pack()
+    tk.Button(column1, text="Alle Spielpläne", padx=10, pady=10, fg="white",
+              bg="#8B0000",
+              width=50,
+              command=lambda reference="allPlans": call_distributor(reference)).pack()
     # Buttons for the gui
 
     root.protocol("WM_DELETE_WINDOW", end_application)
@@ -73,13 +80,18 @@ def create_gui():
 
 
 # Looks whether allGames must be called or TeamGames since it cannot be defined in the buttons themself
-def call_distributor(team, club_name):
-    print(club_name[0])
+def call_distributor(reference):
     wait_animation()
-    if str(club_name[0]) == str(team[1].description):
-        all_games.get_all_games(team[1].id)
+    if reference == "allPlans":
+        all_games.get_all_games(club.id, club.description)
+
+        for i in range(1, len(teams)):
+            team_games.get_team_games(teams.iloc[i].id, teams.iloc[i].description)
     else:
-        team_games.get_team_games(team[1].id)
+        if str(club[0]) == str(reference[1].description):
+            all_games.get_all_games(reference[1].id, club.description)
+        else:
+            team_games.get_team_games(reference[1].id, reference[1].description)
 
     create_gui()
 
@@ -88,7 +100,7 @@ def wait_animation():
     for widget in root.winfo_children():
         widget.destroy()
 
-    canvas = tk.Canvas(root, height=(70 + lines_in_gui(project_variables.update_teams())), width=650)
+    canvas = tk.Canvas(root, height=(100 + lines_in_gui(project_variables.update_teams())), width=650)
     canvas.pack()
 
     loading_frame = tk.Frame(root)
